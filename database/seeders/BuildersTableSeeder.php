82d92f06133fc8bb47f9b3126bda39558ecdea92
Salvation Kemuel <skemuel007@gmail.com>
2022-11-25 00:14:26 +0100

    Seeders json created

--- database/seeders/BuildersTableSeeder.php
+++ database/seeders/BuildersTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Builder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,6 +18,15 @@ class BuildersTableSeeder extends Seeder
         //
         DB::table('builders')->delete();
         $json = File::get("database/data-sample/builders.json");
+        $data = json_decode($json);
+        foreach ($data as $obj) {
+            Builder::create(array(
+                'id' => $obj->id,
+                'name' => $obj->name,
+                'description' => $obj->description,
+                'location' => $obj->location
+            ));
+        }
 
 
     }
